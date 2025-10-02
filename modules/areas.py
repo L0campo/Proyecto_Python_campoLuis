1d7b2c84dc04c9238383121200a057965b9a0cda
L0campo <226594654+L0campo@users.noreply.github.com>
2025-10-02 17:02:16 -0500

    vercion.3

--- modules/areas.py
+++ modules/areas.py
@@ -1,6 +1,11 @@
-rutas= []
+
 def crear_area(nombre_area, capacidad=33):
     pass
+
+        
+
+
+    
 
 def asignar_camper_area(id_camper, nombre_area):
     pass
--- modules/rutas.py
+++ modules/rutas.py
@@ -1,6 +1,38 @@
 rutas=[]
 def crear_ruta(nombre, modulos, sgdb_principal, sgdb_alternativo):
+    programacion=["Java", "JavaScript", "C#"]
+    base_de_datos=["Mysql", "MongoDb" , "Postgresql"]
+    Backend=["NetCore", "Spring Boot", "NodeJS" , "Express"] 
     
+    print("------ Registro de campers ------")
+
+    ID_ruta= input("ingrese el ID de la ruta: ")
+    for ruta in rutas:
+        if ruta["ID"]== ID_ruta:
+            print("ID ya existente")
+            return
+    
+    print("Java, JavaScript, C#")   
+    modulo_Programación= ("ingrese un idioma de los presentados")
+
+    print("Mysql, MongoDb ,Postgresql")
+    modulo_BasesDeDatos= ("ingrese una base de datos de laa presentadas") 
+    
+
+    print("NetCore, Spring Boot, NodeJS , Express")
+    modulo_Backend= ("ingrese un baken de los mostrados")
+
+    fundamentos=("Introducción a la algoritmia", "PSeInt" , "Python")
+    Web=("HTML","CSS", "Bootstrap")
+    modulos=[fundamentos,Web,modulo_Programación,modulo_BasesDeDatos,modulo_Backend]
+
+    ruta = {
+        "ID": ID_ruta,
+        "modulos" :modulos
+    }
+        
+
+
 
 def listar_rutas():
     pass
